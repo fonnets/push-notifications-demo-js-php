5ecb956982fd8a46ebe835b798be8c2171aff6c2
fonnets <90871496+fonnets@users.noreply.github.com>
2023-08-21 19:05:38 +0200

    Update test.php

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ require_once("vendor/autoload.php");
 
 use Minishlink\WebPush\Subscription;
 use Minishlink\WebPush\WebPush;
-?>
+
 $auth = [
     'VAPID' => [
         'subject' => 'mailto:me@website.com', // can be a mailto: or your website address
@@ -15,7 +15,7 @@ $auth = [
 ];
 
 $webPush = new WebPush($auth);
-
+?>
 $report = $webPush->sendOneNotification(
     Subscription::create(json_decode('{"endpoint":"https://fcm.googleapis.com/fcm/send/eoApBqPOGbc:APA91bEO5Rvc3AyR8XFtZNei32q_GpGnlvqPiO1ZkdlC5oaC-clFWA-a63sY1llye8i-M0DMpGriLeQh7d3rbBgrqLlRDaiqf5g3T-P0zYU0v_CkFdzN1wpX-hMnDfys1p5wb8ZljrTe","expirationTime":null,"keys":{"p256dh":"BJhmtryUFtYhamIZ2hWKhqZJYTYizCyCs5rOnIH9USeJ-UbImK989DBvVKJV__84j0pJS-__cPwWDW1yXzkDkZc","auth":"jfc7xzOWBpiGWUBy-v-rHg"}}', ['TTL' => 5000]);
 
