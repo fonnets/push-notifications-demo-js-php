5e04a02a7c4e87fd20abfe4bf5083e4993791af6
fonnets <90871496+fonnets@users.noreply.github.com>
2023-08-21 18:27:29 +0200

    Update send.php

--- api/send.php
+++ api/send.php
@@ -4,6 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+echo 'test'; die();
 require_once("vendor/autoload.php");
 
 use Minishlink\WebPush\Subscription;
