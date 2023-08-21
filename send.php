3d2d00e599ab8a5cf758839dd1a8e8ed3275d0be
fonnets <90871496+fonnets@users.noreply.github.com>
2023-08-21 11:15:09 +0200

    Update send.php
    change notification subscribe

--- send.php
+++ send.php
@@ -16,7 +16,6 @@ $auth = [
 $webPush = new WebPush($auth);
 
 $report = $webPush->sendOneNotification(
-    Subscription::create(json_decode('{"endpoint":"https://fcm.googleapis.com/fcm/send/eOmO_2Z-nHA:APA91bHDoFG8hTpuf11C9t_rVP2IOIOcpDimDLF06DYI9NtRPnQlpRN89KeLP2rl6w8C7WryiEPzkCg3XBwQxmhfL36JNMKGdtNSwd4UegS-7sByuGlYjg6FubHASYrrHVcTfefZaV9W","expirationTime":null,"keys":{"p256dh":"BNBaksmindsZK9u_mghq-Omb1_9bN-hJVP8KjLWB6mlHPf_R3JLmyd-0LwYBGErAjItB2Pex6bAKYFFR_gDdYpo","auth":"H9M9HgHX4a3xmcChKQNWFA"}}',true))
-    , '{"title":"Hi from php" , "body":"php is amazing!" , "url":"./?message=123"}', ['TTL' => 5000]);
+    Subscription::create(json_decode('{"endpoint":"https://fcm.googleapis.com/fcm/send/dzF5MFyjBQE:APA91bHX9XKaZM_CgGDtu4YRT4EPoBEKnQEuI_8PjpwUSvufbzo_4QdKUu7wrS8yTTurwAyKUA3Mv7EoU5SP5rRtw4cwEBy2jIU5U1oNDj0Dfs0Txh7qXvNtCZNr1gQeORy2Q4_PPVsH","expirationTime":null,"keys":{"p256dh":"BFq6AJUlGfRUKhhj7aCndI1SG1rivcHRxfJ62dNkyNtRpmnEvNyFycbmoXvfaY7p9zrLZGwmbJhVk_0SakDs9Kg","auth":"lh2MEA_lwy0jQaxNFc0H6g"}}', ['TTL' => 5000]);
 
     print_r($report);
